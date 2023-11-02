00054069fb14f707b405f176fc6931474843f74b
Aparajita Singh <aparajitalnctscse@gmail.com>
2023-11-02 20:30:47 +0530

    added baseline code

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import time
+from turtle import Turtle,Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+
+screen = Screen()
+screen.bgcolor('black')
+
+
+
+screen.setup(width = 800, height = 600)
+screen.title("Pong game")
+
+
+
+screen.tracer(0)
+
+
+
+
+screen.listen()
+
+
+
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move_right()
+    #detect collision with wall
+    if ball.ycor()>280 or ball.ycor()<-280:
+        ball.bounce_y()
+    #detect collision with paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()< -320:
+        ball.bounce_x()
+    #detect r_paddle misses
+    if ball.xcor()>380:
+        ball.reset_ball()
+        scoreboard.l_point()
+    #detect l_paddle misses
+    if ball.xcor()<-380:
+        ball.reset_ball()
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
